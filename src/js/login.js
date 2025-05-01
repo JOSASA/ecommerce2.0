2b89956faa53532a4874ec54006bff3a3aab9f3f
mexitwice <mexitwice@gmail.com>
2025-05-01 12:02:01 -0700

    login and register + fontawesome,images: no responsive

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+//login para que se voltee a la pagina de registro y viceversa
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
